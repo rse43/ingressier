080d36f6ada52b69827869378a8eb073e4933f01
Shuo Ran <rse43.rse43@gmail.com>
2013-02-06 14:35:54 +1100

    test the xmpp chat support

---
+++ chat.py
@@ -0,0 +1,24 @@
+#!/bin/python
+# -*- coding:utf-8 -*-
+
+import webapp2
+
+from google.appengine.api import xmpp
+
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class XMPPHandler(webapp2.RequestHandler):
+    def post(self):
+        message = xmpp.Message(self.request.POST)
+        if message.body[0:5].lower() == 'hello':
+            message.reply("Greetings!")
+
+app = webapp2.WSGIApplication([('/_ah/xmpp/message/chat/', XMPPHandler)], debug=True)
+
+def real_main():
+    """ main for wsgi app """
+    run_wsgi_app(app)
+
+
+if __name__ == "__main__":
+    real_main()
